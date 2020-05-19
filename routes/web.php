70764b19ebf66b0c0e8cc14434afb48ef9526177
sakina.asano <amaretto.12389@gmail.com>
2020-05-19 12:55:06 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', 'MicropostsController@index');  
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
